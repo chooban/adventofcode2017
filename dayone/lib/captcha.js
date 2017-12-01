f70ecfa068947a1604d20e0fe7dc9e841ac1605f
Ross Hendry <rhendry@googlemail.com>
2017-12-01 15:49:31 +0000

    Bit of refactoring

--- dayone/lib/captcha.js
+++ dayone/lib/captcha.js
@@ -1,39 +1,36 @@
-const solve = (captcha) => {
-  const strInput = '' + captcha;
-  const sequence = strInput + strInput[0];
-  const reducer = (acc, curr, idx, array) => {
-    if (curr == array[idx + 1]) {
-      acc += curr;
-    }
-    return acc;
-  };
+const splitNumber = (num) => {
+  var digits = [];
+  while (num > 0) {
+    digits[digits.length] = num % 10;
+    num = parseInt(num / 10);
+  }
 
-  return sequence
-    .split('')
-    .map((a) => +a)
-    .reduce(reducer, 0);
+  digits.reverse();
+  return digits;
+};
+
+const solve = (captcha) => {
+  const reducer = (acc, curr, idx, array) =>
+    (curr == array[(idx + 1) - (idx == array.length - 1 ? array.length : 0)])
+      ? acc + curr
+      : acc;
+
+  return splitNumber(+captcha).reduce(reducer, 0);
 };
 
 const solvePartTwo = (captcha) => {
-  const increment = ('' + captcha).length / 2;
+  const sequence = splitNumber(+captcha);
+  const increment = sequence.length >> 1;
+
   const reducer = (acc, curr, idx, array) => {
-    const compIdx = idx + increment;
-    if (compIdx >= array.length) {
-      if (curr == array[(compIdx - array.length)]) {
-        acc += curr;
-      }
-    } else {
-      if (curr == array[compIdx]) {
-        acc += curr;
-      }
-    }
-    return acc;
+    const compIdx = idx + (array.length >> 1);
+
+    return (curr == array[compIdx - (compIdx >= array.length ? array.length : 0)])
+      ? acc += curr
+      : acc;
   };
 
-  return ('' + captcha)
-    .split('')
-    .map(a => +a)
-    .reduce(reducer, 0);
+  return sequence.reduce(reducer, 0);
 };
 
 module.exports = {
